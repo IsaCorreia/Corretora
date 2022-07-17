43302cf1ea5b63dffb693adaf3d537d2f7594544
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-07-17 19:58:46 -0300

    feat: :sparkles: Add <errorMiddleware> to <app>
    Custom error handling added to the application

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import errorMiddleware from './middlewares/errorMiddleware';
 import routes from "./routes";
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use(errorMiddleware)
 
 export default app;
