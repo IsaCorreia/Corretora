f671bb062089026808fe3d55d92acbca08099bba
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-07-17 19:30:30 -0300

    feat: :sparkles: Add <database.sql>
    Created basic SQL database to work with.

---
+++ database.sql
@@ -0,0 +1,39 @@
+DROP SCHEMA IF EXISTS Corretora;
+CREATE SCHEMA IF NOT EXISTS Corretora;
+
+CREATE TABLE Corretora.Clientes (
+  CodCliente INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  NomeCliente TEXT NOT NULL,
+  Saldo DECIMAL NOT NULL
+);
+
+CREATE TABLE Corretora.Ativos (
+  CodAtivo INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  NomeAtivo TEXT NOT NULL,
+  QtdeAtivo INTEGER,
+  Valor DECIMAL NOT NULL
+);
+
+CREATE TABLE Corretora.Ativos_Cliente (
+  CodCliente INTEGER,
+  FOREIGN KEY (CodCliente) REFERENCES Corretora.Clientes (CodCliente),
+  CodAtivo INTEGER,
+  FOREIGN KEY (CodAtivo) REFERENCES Corretora.Ativos (CodAtivo),
+  PRIMARY KEY (CodCliente, CodAtivo),
+  QtdeAtivo INTEGER
+);
+
+INSERT INTO
+  Corretora.Clientes (NomeCliente, Saldo)
+VALUES
+  ("Isabela", 100);
+
+INSERT INTO 
+  Corretora.Ativos (NomeAtivo, QtdeAtivo, Valor)
+VALUES
+  ('XP', 10, 17.53);
+
+INSERT INTO
+  Corretora.Ativos_Cliente(CodCliente, CodAtivo, QtdeAtivo)
+VALUES
+  (1,1,1);
