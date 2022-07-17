49e2131eea3d195dd0869edc539da206c9831927
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-07-17 19:47:56 -0300

    feat: :sparkles: Add <routes>
    Pre-mounting routes to be implemented later

---
+++ src/routes.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+const routes = Router();
+
+routes.use( '/investimentos', ()=>{} );
+routes.use( '/ativos', ()=>{} );
+routes.use( '/conta', () => { } );
+
+export default routes;
