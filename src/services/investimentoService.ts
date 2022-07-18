731621e59964eb44c2216447e08916c14a69d813
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-07-18 00:49:56 -0300

    feat: :construction: Add asset updates to <buyAssets> at <investimentoService>
    Gets asset quantity, calculates the quantity to be decreased and updates the stock.

--- src/services/investimentoService.ts
+++ src/services/investimentoService.ts
@@ -5,6 +5,11 @@ const buyAssets = async ( req: Request ) => {
   const { CodCliente, CodAtivo, QtdeAtivo } = req.body;
   await investimentoModel.addPurchase( CodCliente, CodAtivo, QtdeAtivo );
 
+  // Recupera informações do ativo para atualizar o estoque da Corretora
+  const [assetInfo] = await investimentoModel.getAsset( CodAtivo );
+  const newAssetStock = Number(assetInfo.QtdeAtivo - QtdeAtivo);
+  await investimentoModel.updateAssetStock( newAssetStock, CodAtivo );
+
 }
 
 export default {
