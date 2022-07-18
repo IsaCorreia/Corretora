36762bd9c777bc39e3b1438dc152f841c9c59411
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-07-18 00:56:14 -0300

    feat: :sparkles: Add client balance update to <buyAssets> at <investimentoService>
    Get client balance, calculates value to be decreased and updates the balance.

--- src/services/investimentoService.ts
+++ src/services/investimentoService.ts
@@ -1,8 +1,10 @@
 import { Request } from 'express';
+import clienteModel from '../models/clienteModel';
 import investimentoModel from '../models/investimentoModel';
 
 const buyAssets = async ( req: Request ) => {
   const { CodCliente, CodAtivo, QtdeAtivo } = req.body;
+  // Adiciona a compra ao registro
   await investimentoModel.addPurchase( CodCliente, CodAtivo, QtdeAtivo );
 
   // Recupera informações do ativo para atualizar o estoque da Corretora
@@ -10,6 +12,11 @@ const buyAssets = async ( req: Request ) => {
   const newAssetStock = Number(assetInfo.QtdeAtivo - QtdeAtivo);
   await investimentoModel.updateAssetStock( newAssetStock, CodAtivo );
 
+  // Recupera saldo Cliente para débito
+  const [clientInfo] = await clienteModel.getClient( CodCliente );  
+  const newClientBalance = Number(clientInfo.Saldo) - (Number(assetInfo.Valor) * QtdeAtivo);
+  await clienteModel.decreaseClientBalance( newClientBalance, CodCliente );
+  
 }
 
 export default {
