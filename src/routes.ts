e83b0fd3837d0ded0b66e506837d5e1046a11298
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-07-18 00:39:24 -0300

    feat: :sparkles: Add <investimentoController> to <routes>
    </investimentos> route now uses <investimentoController>

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import clienteController from './controllers/clienteController';
+import investimentoController from './controllers/investimentoController';
 
 const routes = Router();
 
-routes.use( '/investimentos', ()=>{} );
+routes.use( '/investimentos', investimentoController );
 routes.use( '/ativos', clienteController );
 routes.use( '/conta', () => { } );
 
