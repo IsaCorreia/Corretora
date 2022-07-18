91e9b0278ac80bfb70bfba4bac7cbc58c880cc4a
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-07-17 23:23:54 -0300

    feat: :sparkles: Add <connection> to Model layer
    Uses environment variables set inside the Docker container

---
+++ src/models/connection.ts
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+import mysql from 'mysql2/promise';
+dotenv.config();
+
+export default mysql.createPool( {
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD
+} );
