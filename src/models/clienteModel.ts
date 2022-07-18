794373bc83745d13acb8ea81d306d0a9af2f90f9
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-07-18 00:50:55 -0300

    feat: :sparkles: Add <getClient> to <clientModel>
    Gets Client info

--- src/models/clienteModel.ts
+++ src/models/clienteModel.ts
@@ -1,4 +1,11 @@
+import IClient from '../interfaces/IClient';
 import connection from './connection';
+
+const getClient = async ( CodCliente: number ): Promise<IClient[]> => {
+  const getQuery = `SELECT * FROM Corretora.Clientes WHERE CodCliente = ?;`;
+  const [ rows ] = await connection.execute( getQuery, [ CodCliente ] );
+  return rows as IClient[];
+}
 
 const getAssetsByClient = async( id: number ) => {
   const getAssetsQuery = `SELECT 
