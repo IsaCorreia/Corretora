6082fcc9ca99220d2e249891672c3748eb65d7cc
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-07-19 20:39:55 -0300

    feat: :sparkles: Add <getAllAssets> to <investimentoModel>
    Selects all assets in stock

--- src/models/investimentoModel.ts
+++ src/models/investimentoModel.ts
@@ -2,6 +2,12 @@ import { ResultSetHeader } from 'mysql2';
 import IAsset from '../interfaces/IAsset';
 import connection from './connection';
 
+const getAllAssets = async (): Promise<IAsset[]> => {
+  const getQuery = `SELECT * FROM Corretora.Ativos;`;
+  const [ rows ] = await connection.execute( getQuery );
+  return rows as IAsset[];
+}
+
 const addPurchase = async ( CodCliente: number, CodAtivo: number, QtdeAtivo: number ): Promise<ResultSetHeader> => {
   // Adicionar 'CodCliente','CodAtivo','QtdeAtivo' à 'Ativos_Cliente'
   const addQuery = `INSERT INTO Corretora.Ativos_Cliente(CodCliente, CodAtivo, QtdeAtivo) VALUES (?, ?, ?);`;
@@ -21,6 +27,7 @@ const updateAssetStock = async ( QtdeAtivo: number, CodAtivo: number ): Promise<
   return result as ResultSetHeader;
 }
 export default {
+  getAllAssets,
   addPurchase,
   getAsset,
   updateAssetStock,
