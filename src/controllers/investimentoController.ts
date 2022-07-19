dbaee4c1d9de26e8710025b3249e8268ff70850d
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-07-19 20:44:33 -0300

    feat: :sparkles: Add GET</investimentos/> route to <investimentoController>
    Returns all assets in stock

--- src/controllers/investimentoController.ts
+++ src/controllers/investimentoController.ts
@@ -4,6 +4,11 @@ import investimentoService from '../services/investimentoService';
 
 const investimentoController = Router();
 
+investimentoController.get( '/', async ( req: Request, res: Response ): Promise<Response> => {
+  const assets = await investimentoService.getAssets();
+  return res.status(OK).json(assets)
+ })
+
 investimentoController.post( '/comprar', async ( req: Request, res: Response ) => {
   await investimentoService.buyAssets( req );
   return res.status( CREATED ).json({message: 'Ações compradas'});
