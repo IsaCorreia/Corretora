ab3452ee834e173182c319cec7d47bbd74724f53
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-07-20 23:15:29 -0300

    refactor: :recycle: Changes to <validateAssetStock>
    Changes 'return response' to 'throw new Error', as error middleware now functions as expected

--- src/middlewares/validateAssetStock.ts
+++ src/middlewares/validateAssetStock.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { BAD_REQUEST } from "http-status-codes";
 import IPurchaseRequest from "../interfaces/IPurchaseRequest";
 import investimentoModel from "../models/investimentoModel";
+import HttpException from "../utils/HttpException";
 
 export default async function validateAssetStock(
   req: Request,
@@ -10,14 +11,14 @@ export default async function validateAssetStock(
 ): Promise<void | Response> {
   const { CodAtivo, QtdeAtivo } = req.body as IPurchaseRequest;
   const [asset] = await investimentoModel.getAsset(Number(CodAtivo));
-  if ( asset === undefined ) {
-    return res.status( BAD_REQUEST ).json( "O ativo solicitado não existe" );
+  if (asset === undefined) {
+    throw new HttpException(BAD_REQUEST, "O ativo solicitado não existe");
   }
-  if (asset && (asset.QtdeAtivo === 0 || asset.QtdeAtivo < QtdeAtivo)){
-    return res
-      .status(BAD_REQUEST)
-      .json(
-        `O ativo '${asset.NomeAtivo}' não tem quantidade suficiente disponível`
-      );}
+  if (asset && (asset.QtdeAtivo === 0 || asset.QtdeAtivo < QtdeAtivo)) {
+    throw new HttpException(
+      BAD_REQUEST,
+      `O ativo '${asset.NomeAtivo}' não tem quantidade suficiente disponível`
+    );
+  }
   next();
 }
