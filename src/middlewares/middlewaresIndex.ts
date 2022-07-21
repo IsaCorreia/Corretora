48869262480f35ee1f59fa92c1e086222886cd13
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-07-20 23:17:18 -0300

    refactor: :recycle: Add <middlewareIndex>
    Concentrating all middleware imports to this file

---
+++ src/middlewares/middlewaresIndex.ts
@@ -0,0 +1,4 @@
+import errorMiddleware from './errorMiddleware';
+import validateAssetRequest from './validateAssetRequest';
+import validateAssetStock from './validateAssetStock';
+export default { errorMiddleware, validateAssetRequest, validateAssetStock };
