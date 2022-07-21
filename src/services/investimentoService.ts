5fdfe6a160a3b2acea9e5a0772025432888a5b4a
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-07-21 00:18:16 -0300

    refactor: :recycle: Improve <buyAssets> at <investimentoService>
    It now checks if there is already a registry of the client/asset combination, and updates it, or adds a new registry. decreaseClientBalance renamed for better use.

--- src/services/investimentoService.ts
+++ src/services/investimentoService.ts
@@ -1,7 +1,8 @@
-import { Request } from 'express';
-import IAsset from '../interfaces/IAsset';
-import clienteModel from '../models/clienteModel';
-import investimentoModel from '../models/investimentoModel';
+import { Request } from "express";
+import IAsset from "../interfaces/IAsset";
+import IPurchase from '../interfaces/IPurchase';
+import clienteModel from "../models/clienteModel";
+import investimentoModel from "../models/investimentoModel";
 
 const getAssets = async (): Promise<IAsset[]> => {
   const assets = await investimentoModel.getAllAssets();
@@ -12,7 +13,36 @@ const getAssetById = async ( req: Request ): Promise<IAsset> => {
   const { id } = req.params;
   const [asset] = await investimentoModel.getAsset(Number(id));
   return asset;
-}
+};
+
+const buyAssets = async (req: Request): Promise<void> => {
+  const { CodCliente, CodAtivo, QtdeAtivo } = req.body as IPurchase;
+  // Adiciona a compra ao registro
+  const [purchaseInfo] = await investimentoModel.getPurchase(
+    CodCliente,
+    CodAtivo
+  );
+  if (purchaseInfo === undefined) {
+    await investimentoModel.addPurchase(CodCliente, CodAtivo, QtdeAtivo);
+  } else {
+    const newPurchaseRegistry:number = purchaseInfo.QtdeAtivo + QtdeAtivo;
+    await investimentoModel.updatePurchase(
+      CodCliente,
+      CodAtivo,
+      newPurchaseRegistry
+    );
+  }
+  // Recupera informações do ativo para atualizar o estoque da Corretora
+  const [assetInfo] = await investimentoModel.getAsset(CodAtivo);
+  const newAssetStock = assetInfo.QtdeAtivo - QtdeAtivo;
+  await investimentoModel.updateAssetStock(newAssetStock, CodAtivo);
+
+  // Recupera saldo Cliente para débito
+  const [clientInfo] = await clienteModel.getClient(CodCliente);
+  const newClientBalance =
+    Number(clientInfo.Saldo) - (Number(assetInfo.Valor) * QtdeAtivo);
+  await clienteModel.updateClientBalance(newClientBalance, CodCliente);
+};
 
 const buyAssets = async ( req: Request ): Promise<void> => {
   const { CodCliente, CodAtivo, QtdeAtivo } = req.body;
