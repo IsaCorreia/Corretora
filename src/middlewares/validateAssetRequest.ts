6dbc59e16bffa3865b615e28c5a2cf37633c37c7
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-07-20 23:16:28 -0300

    feat: :sparkles: Add <validateAssetRequest>
    Created validation middleware to check the request body on </investimentos/comprar> and </investimentos/vender> routes

---
+++ src/middlewares/validateAssetRequest.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+import { BAD_REQUEST } from "http-status-codes";
+import Joi from "joi";
+import HttpException from "../utils/HttpException";
+
+const schema = Joi.object({
+  CodCliente: Joi.number().required(),
+  CodAtivo: Joi.number().required(),
+  QtdeAtivo: Joi.number().required(),
+});
+
+export default function validateAssetRequest(
+  req: Request,
+  _res: Response,
+  next: NextFunction
+) {
+  const { error } = schema.validate(req.body);
+  if (error !== undefined)
+    throw new HttpException(BAD_REQUEST, error.details[0].message);
+  next();
+}
