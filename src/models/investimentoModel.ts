1cbe405bd91bd7cdb74c532e3d551679d6a716b0
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-07-21 00:02:56 -0300

    feat: :sparkles: Add <getPurchase> to <investimentoModel>

--- src/models/investimentoModel.ts
+++ src/models/investimentoModel.ts
@@ -1,6 +1,7 @@
-import { ResultSetHeader } from 'mysql2';
-import IAsset from '../interfaces/IAsset';
-import connection from './connection';
+import { ResultSetHeader } from "mysql2";
+import IAsset from "../interfaces/IAsset";
+import IPurchase from "../interfaces/IPurchase";
+import connection from "./connection";
 
 const getAllAssets = async (): Promise<IAsset[]> => {
   const getQuery = `SELECT * FROM Corretora.Ativos;`;
@@ -8,7 +9,20 @@ const getAllAssets = async (): Promise<IAsset[]> => {
   return rows as IAsset[];
 }
 
-const addPurchase = async ( CodCliente: number, CodAtivo: number, QtdeAtivo: number ): Promise<ResultSetHeader> => {
+const getPurchase = async (
+  CodCliente: number,
+  CodAtivo: number
+): Promise<IPurchase[]> => {
+  const getQuery = `SELECT * FROM Corretora.Ativos_Cliente WHERE CodCliente = ? AND CodAtivo = ?;`;
+  const [rows] = await connection.execute(getQuery, [CodCliente, CodAtivo]);
+  return rows as IPurchase[];
+};
+
+const addPurchase = async (
+  CodCliente: number,
+  CodAtivo: number,
+  QtdeAtivo: number
+): Promise<ResultSetHeader> => {
   // Adicionar 'CodCliente','CodAtivo','QtdeAtivo' à 'Ativos_Cliente'
   const addQuery = `INSERT INTO Corretora.Ativos_Cliente(CodCliente, CodAtivo, QtdeAtivo) VALUES (?, ?, ?);`;
   const [ result ] = await connection.execute( addQuery, [ CodCliente, CodAtivo, QtdeAtivo ] );
@@ -28,6 +42,7 @@ const updateAssetStock = async ( QtdeAtivo: number, CodAtivo: number ): Promise<
 }
 export default {
   getAllAssets,
+  getPurchase,
   addPurchase,
   getAsset,
   updateAssetStock,
