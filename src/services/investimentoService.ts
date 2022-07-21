2671ee1d36f6c1ce3733411b6a0a098ab9e206eb
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-07-20 23:25:52 -0300

    style: :art: Changes to <investimentoService>
    Reorder functions

--- src/services/investimentoService.ts
+++ src/services/investimentoService.ts
@@ -3,6 +3,17 @@ import IAsset from '../interfaces/IAsset';
 import clienteModel from '../models/clienteModel';
 import investimentoModel from '../models/investimentoModel';
 
+const getAssets = async (): Promise<IAsset[]> => {
+  const assets = await investimentoModel.getAllAssets();
+  return assets;
+}
+
+const getAssetById = async ( req: Request ): Promise<IAsset> => {
+  const { id } = req.params;
+  const [asset] = await investimentoModel.getAsset(Number(id));
+  return asset;
+}
+
 const buyAssets = async ( req: Request ): Promise<void> => {
   const { CodCliente, CodAtivo, QtdeAtivo } = req.body;
   // Adiciona a compra ao registro
@@ -17,20 +28,7 @@ const buyAssets = async ( req: Request ): Promise<void> => {
   const [clientInfo] = await clienteModel.getClient( CodCliente );  
   const newClientBalance = Number(clientInfo.Saldo) - (Number(assetInfo.Valor) * QtdeAtivo);
   await clienteModel.decreaseClientBalance( newClientBalance, CodCliente );
-  
 }
-
-const getAssets = async (): Promise<IAsset[]> => {
-  const assets = await investimentoModel.getAllAssets();
-  return assets;
-}
-
-const getAssetById = async ( req: Request ): Promise<IAsset> => {
-  const { id } = req.params;
-  const [asset] = await investimentoModel.getAsset(Number(id));
-  return asset;
-}
-
 
 export default {
   getAssets,
