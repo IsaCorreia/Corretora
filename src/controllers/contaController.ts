2e89dfb36425c3587b85defbb61442c1da5df101
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-07-22 00:11:24 -0300

    feat: :sparkles: Add <contaController> to <routes>

---
+++ src/controllers/contaController.ts
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+
+const contaController = Router();
+
+export default contaController;
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import clienteController from './controllers/clienteController';
+import contaController from './controllers/contaController';
 import investimentoController from './controllers/investimentoController';
 
 const routes = Router();
 
 routes.use( '/investimentos', investimentoController );
 routes.use( '/ativos', clienteController );
-routes.use( '/conta', () => { } );
+routes.use( '/conta', contaController );
 
 export default routes;
