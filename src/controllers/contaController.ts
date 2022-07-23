bdb25848be2dae182b5a6e2d54de9eea74f9bdea
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-07-22 22:17:35 -0300

    feat: :sparkles: Add GET</conta/:id> route
    Returns the client's current balance.

--- src/controllers/contaController.ts
+++ src/controllers/contaController.ts
@@ -1,5 +1,16 @@
-import { Router } from 'express';
+import { Request, Response, Router } from "express";
+import { CREATED, OK } from "http-status-codes";
+import contaService from "../services/contaService";
 
 const contaController = Router();
+
+contaController.get(
+  "/:id",
+  /* authClient, */
+  async (req: Request, res: Response): Promise<Response> => {
+    const balance = await contaService.getBalance(req);
+    return res.status(OK).json(balance);
+  }
+);
 
 export default contaController;
