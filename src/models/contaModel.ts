76fd9420ca5254a1d70979c12af28fac466efe0b
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-07-22 22:02:05 -0300

    feat: :sparkles: Add <updateBalance> to <clientModel>

--- src/models/contaModel.ts
+++ src/models/contaModel.ts
@@ -1,10 +1,19 @@
-import IClient from '../interfaces/IClient';
-import connection from './connection';
+import { ResultSetHeader } from "mysql2";
 
 const getBalance = async ( id: number ): Promise<IClient[]> => {
   const getQuery = `SELECT * FROM Corretora.Clientes WHERE CodCliente = ?;`;
   const [ rows ] = await connection.execute( getQuery, [ id ] );
   return rows as IClient[];
-}
+
+const updateBalance = async (
+  CodCliente: number,
+  Valor: number
+): Promise<ResultSetHeader> => {
+  const updateQuery = `UPDATE Corretora.Clientes SET Saldo = ? WHERE CodCliente = ?;`;
+  const [result] = await connection.execute(updateQuery, [Valor, CodCliente]);
+  return result as ResultSetHeader;
+};
+
+export default { getBalance, updateBalance };
 
 export default { getBalance }
