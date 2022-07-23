bcc29b68cad699ba3b38825948e6bba4e2d9f7ee
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-07-22 23:34:20 -0300

    refactor: :recycle: Changes to <buyAssets> at <investimentoService>
    Add client balance check to prevent asset purchase without enough funds

--- src/services/investimentoService.ts
+++ src/services/investimentoService.ts
@@ -1,8 +1,11 @@
 import { Request } from "express";
+import { UNPROCESSABLE_ENTITY } from 'http-status-codes';
 import IAsset from "../interfaces/IAsset";
 import IPurchase from "../interfaces/IPurchase";
 import clienteModel from "../models/clienteModel";
 import investimentoModel from "../models/investimentoModel";
+import checkBalance from '../utils/checkBalance';
+import HttpException from '../utils/HttpException';
 
 const getAssets = async (): Promise<IAsset[]> => {
   const assets = await investimentoModel.getAllAssets();
@@ -17,6 +20,20 @@ const getAssetById = async (req: Request): Promise<IAsset> => {
 
 const buyAssets = async (req: Request): Promise<void> => {
   const { CodCliente, CodAtivo, QtdeAtivo } = req.body as IPurchase;
+  // Recupera saldo Cliente e estoque de ativos
+  const [assetInfo] = await investimentoModel.getAsset(CodAtivo);
+  const [clientInfo] = await clienteModel.getClient(CodCliente);
+  
+  const transactionValue: number = assetInfo.QtdeAtivo * QtdeAtivo;
+  const canPurchase: boolean = await checkBalance( CodCliente, transactionValue );
+  if ( !canPurchase ) throw new HttpException( UNPROCESSABLE_ENTITY, "Saldo insuficiente" );
+
+  const newAssetStock = assetInfo.QtdeAtivo - QtdeAtivo;
+  const newClientBalance =
+  Number(clientInfo.Saldo) - transactionValue;
+  await investimentoModel.updateAssetStock(newAssetStock, CodAtivo);
+  await clienteModel.updateClientBalance( newClientBalance, CodCliente );
+  
   // Adiciona a compra ao registro
   const [purchaseInfo] = await investimentoModel.getPurchase(
     CodCliente,
@@ -33,16 +50,6 @@ const buyAssets = async (req: Request): Promise<void> => {
       newPurchaseRegistry
     );
   }
-  // Recupera informações do ativo para atualizar o estoque da Corretora
-  const [assetInfo] = await investimentoModel.getAsset(CodAtivo);
-  const newAssetStock = assetInfo.QtdeAtivo - QtdeAtivo;
-  await investimentoModel.updateAssetStock(newAssetStock, CodAtivo);
-
-  // Recupera saldo Cliente para débito
-  const [clientInfo] = await clienteModel.getClient(CodCliente);
-  const newClientBalance =
-    Number(clientInfo.Saldo) - Number(assetInfo.Valor) * QtdeAtivo;
-  await clienteModel.updateClientBalance(newClientBalance, CodCliente);
 };
 
 const sellAssets = async (req: Request): Promise<void> => {
