8687b75d0db4e93e41c40bb5ac931362836191b4
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-07-24 22:17:07 -0300

    refactor: :recycle: Deleteing routes related to <clienteController>

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express';
-import clienteController from './controllers/clienteController';
 import contaController from './controllers/contaController';
 import investimentoController from './controllers/investimentoController';
 
 const routes = Router();
 
 routes.use( '/investimentos', investimentoController );
-routes.use( '/ativos', clienteController );
 routes.use( '/conta', contaController );
 
 export default routes;
